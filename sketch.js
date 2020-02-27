b23e6f9eb655ac6fcc10284689271c46bb426c36
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-02-26 16:08:55 -0800

    2/26 update 4pm

--- sketch.js
+++ sketch.js
@@ -1,10 +1,37 @@
+let students =[{
+ name: "jake",
+ food: "cake"
+}, {
+  name: "blake",
+  food: "shake"
+}, {
+  name: "sam",
+  food: "ham"
+}, {
+  name: "cam",
+  food: "pizza"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  textSize(32);
+  text("click to start", width/2, height/2);
 }
 
 function draw() {
-  background(220);
-  textSize(10);
-  fill(0);
-  text('Randomizer will go here! xD', width/2, height/2);
+}
+
+function mousePressed() {
+if (students[0]){
+background(random(200,255));
+randomIndex = int(random(students.length));
+text(students[randomIndex].name,50,50);
+students.splice(randomIndex,1);
+} else {
+  background(random(255));
+  text("nothing left",50,50);
+}
 }
