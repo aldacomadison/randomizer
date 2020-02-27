9c8b3575992e06898327753b556640ebecfc629e
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-02-26 22:14:03 -0800

    edits 2/26 10pm

---
+++ setlist.js
@@ -0,0 +1,37 @@
+let setlist =[{
+ artist: "Alison Wonderland",
+ song: "Church"
+}, {
+  artist: "Seven Lions",
+  song: "First Time"
+}, {
+  artist: "Zeds Dead",
+  song: "Frontlines"
+}, {
+  artist: "TroyBoi",
+  song: "Grimey"
+}, {
+  artist: "Tiesto",
+  song: "Jackie Chan"
+}, {
+  artist: "Kayzo",
+  song: "Dark Skies"
+}, {
+  artist: "Slander",
+  song: "FEELING GUD"
+}, {
+  artist: "Yellow Claw",
+  song: "DJ Turn It Up"
+}, {
+  artist: "NGHTMRE",
+  song: "CASH COW"
+}, {
+  artist: "JAUZ",
+  song: "Feel The Volume"
+}, {
+  artist: "DJ Snake",
+  song: "Middle"
+}, {
+  artist: "Snakeships",
+  song: "All My Friends"
+}];
--- sketch.js
+++ sketch.js
@@ -1,35 +1,49 @@
-let students =[{
- name: "jake",
- food: "cake"
-}, {
-  name: "blake",
-  food: "shake"
-}, {
-  name: "sam",
-  food: "ham"
-}, {
-  name: "cam",
-  food: "pizza"
-}];
-
+// let students =[{
+//  name: "jake",
+//  food: "cake"
+// }, {
+//   name: "blake",
+//   food: "shake"
+// }, {
+//   name: "sam",
+//   food: "ham, candy, donuts"
+// }, {
+//   name: "cam",
+//   food: "pizza"
+// }];
+//let setlist;
 let randomIndex;
+let button;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
   textSize(32);
-  text("click to start", width/2, height/2);
+  //text("click to start", width/2, height/2);
+
+  button = createButton('click to start randomizer');
+  button.position(50,50);
+  button.mousePressed(greet);
 }
 
 function draw() {
 }
+function greet(){
+  if (setlist[0]){
+  background(random(200,255));
+  randomIndex = int(random(setlist.length));
+  text(setlist[randomIndex].artist,50,50);
+  setlist.splice(randomIndex,1);
+  }
+   button.remove();
+}
 
 function mousePressed() {
-if (students[0]){
+if (setlist[0]){
 background(random(200,255));
-randomIndex = int(random(students.length));
-text(students[randomIndex].name,50,50);
-students.splice(randomIndex,1);
+randomIndex = int(random(setlist.length));
+text(setlist[randomIndex].artist,50,50);
+setlist.splice(randomIndex,1);
 } else {
   background(random(255));
   text("nothing left",50,50);
