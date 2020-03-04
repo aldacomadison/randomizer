e5687febfcad6292b10e9264c20b3c3e036537d1
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-03-03 22:37:00 -0800

    updates 3/3

--- setlist.js
+++ setlist.js
@@ -1,37 +1,37 @@
 let setlist =[{
  artist: "Alison Wonderland",
- song: "Church"
+ song: ["Church", "NO","Peace","Easy","I Want U"]
 }, {
   artist: "Seven Lions",
-  song: "First Time"
+  song: ["First Time","Need Your Love","Rush Me Over","Strangers","Worlds Apart"]
 }, {
   artist: "Zeds Dead",
-  song: "Frontlines"
+  song: ["Frontlines","Neck And Neck","Save My Grave","Stars Tonight","Blame"]
 }, {
   artist: "TroyBoi",
-  song: "Grimey"
+  song: ["Grimey","Do You?","Afterhours","Solid","O.G"]
 }, {
   artist: "Tiesto",
-  song: "Jackie Chan"
+  song: ["Jackie Chan","Blue","BOOM","Ritual","WOW"]
 }, {
   artist: "Kayzo",
-  song: "Dark Skies"
+  song: ["Dark Skies","WakeUp","Without You","Braincase","Alone"]
 }, {
   artist: "Slander",
-  song: "FEELING GUD"
+  song: ["FEELING GUD","GUD VIBRATIONS","Superhuman","Love is Gone","Potions"]
 }, {
   artist: "Yellow Claw",
-  song: "DJ Turn It Up"
+  song: ["DJ Turn It Up","Crash This Party","Love and War","Wild Mustang","Home"]
 }, {
   artist: "NGHTMRE",
-  song: "CASH COW"
+  song: ["CASH COW","STREET","Bruises","Feel Your Love","MVP"]
 }, {
   artist: "JAUZ",
-  song: "Feel The Volume"
+  song: ["Feel The Volume","I Dare You","Rock The Party","Magic","Deeper Love"]
 }, {
   artist: "DJ Snake",
-  song: "Middle"
+  song: ["Middle","Loco Contigo","A Different Way","Try Me","Taki Taki"]
 }, {
   artist: "Snakeships",
-  song: "All My Friends"
+  song: ["All My Friends","Summer Fade","Cruel","Either Way","Forever"]
 }];
--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 let randomIndex;
 let button;
 let animating = false;
+let btn = false;
 
 function setup() {
   createCanvas(500, 500);
   background(220);
   fill(0);
   textSize(40);
-  //text("click to start", width/2, height/2);
+  noStroke();
 
+  //decorate start screen
   button = createButton('Welcome to EDC 2020');
   button.position(175,225);
   button.mousePressed(greet);
@@ -16,40 +18,59 @@ function setup() {
 
 function draw() {
 
-if(animating == true){
-  fill(random(255),random(255),random(255),random(255));
-  rect(random(width), random(height), random(50), 100);
-}
-
-
-}
-function greet(){
-  if (setlist[0]){
-  background(200);
-  randomIndex = int(random(setlist.length));
-  text(setlist[randomIndex].artist,50,50);
-  setlist.splice(randomIndex,1);
+  if(animating == true){
+    noStroke();
+    fill(random(255),random(255),random(255),random(255));
+    rect(random(width), random(height), random(50), 100);
   }
-   button.remove();
+  else if(btn==false) {
+    let randR;
+    let randG;
+    let randB;
+    let randA;
+    for (let i = 0; i < 500; i += 50) {
+      for (let j = 0; j < 500; j += 50) {
+          randR = random(255);
+          randG = random(255);
+          randB = random(255);
+          randA = random(200, 255);
+          fill(randR, randG, randB, randA);
+          rect( i, j, 20, 20 );
+        }
+      }
+    }
 }
+
+function greet(){
+
+  button.remove();
+  btn = true;
+}
+
 function randomizer(){
   animating = false;
-
+  clear();
+  let date = ["Friday", "Saturday", "Sunday"]
   if (setlist[0]){
   background(random(255),random(255),random(255),random(255));
   randomIndex = int(random(setlist.length));
-  text(setlist[randomIndex].artist,175,225);
-  text(setlist[randomIndex].song,200,250);
+  fill(0);
+  textAlign(CENTER);
+  text(setlist[randomIndex].artist,145,200);
+  text(random(setlist[randomIndex].song),250,250);
+  text(random(date),300,300);
   setlist.splice(randomIndex,1);
 
   } else {
     background(random(255));
     text("nothing left",50,50);
   }
-
 }
-function mousePressed() {
-  animating = true;
-  setTimeout(randomizer,1000);
 
+function mousePressed() {
+  if(btn==true)
+  {
+    animating = true;
+    setTimeout(randomizer,1000);
+  }
 }
